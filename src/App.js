e68f413de826bd535c1320431e53d57ee2b5a8a1
Siripong <boomlee2014@gmail.com>
2025-12-25 10:53:31 +0700

    add function component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
-import './App.css';
-import './style.css';
+import { Content, Footer, Header } from './func-components.js';
 
 function App() {
   return (
     <>
-      <h2>DeveloperThai.com</h2>
-      <div className='title'>React & React Native</div>
+      <Header/>
+      <Content/>
+      <Footer/>
     </>
   )
 }
