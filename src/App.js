3ddd83c4aaef2125b2e90d3f6ea484c7c733d366
Siripong <boomlee2014@gmail.com>
2025-12-25 11:26:06 +0700

    add Class-components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import { Content, Footer, Header } from './func-components.js';
+import Calendar from './class-components';
 
 function App() {
   return (
     <>
       <Header/>
+      <p><center><Calendar/></center></p>
       <Content/>
       <Footer/>
     </>
