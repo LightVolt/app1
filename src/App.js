41dd9bebfacf165b5aa1b863372a9379e11c6601
Siripong <boomlee2014@gmail.com>
2026-01-29 10:13:22 +0700

    Context

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import MessageBox from './state-func';
+import { userContext } from "./context";
+import React from "react";
+import Content from "./context-content";
 
 function App() {
-  return <MessageBox/>
+  return(
+    <userContext.Provider value={'Tom Jerry'}>
+      <Content/>
+    </userContext.Provider>
+  )
 }
 
 export default App;
